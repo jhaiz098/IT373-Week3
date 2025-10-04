ecaee13dbecfa0eb0eea74f1b60d0aa03d0f22e3
James <james.fernandez1239@gmail.com>
2025-10-05 05:04:14 +0800

    added models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200, validators=[MinLengthValidator(3)]) 
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, validators=[MinLengthValidator(3)]) 
+
+    def __str__(self):
+        return self.name
